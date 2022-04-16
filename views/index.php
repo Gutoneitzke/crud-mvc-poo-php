e32e63becc8ee22500eb53a3a0e670d7cb05f111
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-04-16 13:59:45 -0300

    styles adjust and new svg

--- views/index.php
+++ views/index.php
@@ -9,8 +9,14 @@
 </head>
 <body>
     <?php if(!empty($returnMessage)): ?>
-        <p><?= $returnMessage ?></p>
-        <button onclick="reload()">x</button>
+        <div class="box-message">
+            <p><?= $returnMessage ?></p>
+            <button class="btn-close-message" onclick="reload()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
+        </div>
     <?php endif; ?>
     <h1>Clients List | GN</h1>
     <form action="index.php">
